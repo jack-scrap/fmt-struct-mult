b66a6a215463564eadc23f38b83095f51b44ad39
Jack <jackhasakeyboard@gmail.com>
2023-10-05 18:27:31 -0600

    Output number postfixed signifying that it's a positive integer

--- main.c
+++ main.c
@@ -1,3 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void err(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc != 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	int n = atoi(argv[1]);
+
+	if (n == 0) {
+		err("Value zero or non-numeric argument");
+
+		return 1;
+	}
+
+	if (n < 0) {
+		err("Value negative");
+
+		return 1;
+	}
+
+	printf("%dx", n);
+
 	return 0;
 }
